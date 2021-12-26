ec456f53ccece68ce46037afa7253f20d01185c8
Величайший <@DESKTOP-84DLJN9>
2021-12-26 15:17:31 +0300

    Добавьте файлы проекта.

---
+++ regtr/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace regtr
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+
+        {
+            InitializeComponent();
+
+
+
+        }
+
+        //При нажатие проверяется данные для входа,если они правильные то подключаемся к личному кабинету
+        private void Button_Click_Vhod(object sender, RoutedEventArgs e)
+        {
+            // инициал. переменных
+            string log = LoginAuth.Text;
+            string pass = PassAuth.Password;
+            
+            //Условие,если длина логина и пароля больше 2 символов,идет подключение к базе
+            if (log.Length > 2 && pass.Length > 3)
+            {
+                using (hachiuebani123 hachi = new hachiuebani123())
+                    //Проверка на правильные данные пользователя
+                {
+                    //запрос где логин и пароль совпадают с данными в базе
+                    var query = hachi.auth.Where(x => x.login == log 
+                                                 && x.password == pass).FirstOrDefault();
+                    //условие если запрос не пустой открывается личный кабинет
+                    if (query != null)
+                    {
+                        MessageBox.Show("Вы успешно зашли!");
+                        Window3 profile = new Window3();
+                        this.Close();
+                        profile.Show();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Не правильно введен логин или пароль");
+
+            }
+        }
+        //При нажатие на кнопку открывается окно регистрации
+        private void Button_Click_Reg(object sender, RoutedEventArgs e)
+        {
+            Window2 reg = new Window2();
+            this.Close();
+            reg.Show();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//https://studbooks.net/1938603/informatika/proektirovanie_bazy_dannyh_subd_mysql
---
+++ regtr/Window2.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace regtr
+{
+    /// <summary>
+    /// Логика взаимодействия для Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+        //При нажатие на кнопку идет создание нового пользователя
+        private void Button_Click_Register(object sender, RoutedEventArgs e)
+        {
+
+            {
+                Window2 reg = new Window2();
+                this.Close();
+                reg.Show();
+                //Ввод логина и пароля для нового пользователя 
+                string loginReg = RegLog.Text.Trim();
+                string passReg = RegPass.Password;
+                string passReg2 = RegPassR.Password;
+                if (loginReg.Length > 2 && passReg.Length > 5 && passReg2.Length > 5)
+
+                {
+                    if (!Regex.IsMatch(loginReg.ToLower(), "[йцукенгшщзхъфывапролджэячсмитьбю.\\||//ъх!@#$%^&*()_+=-Ё~`:;\"\'<>,-@№{}[]"))
+                    {
+
+                        //условие,если пароль и подтв пароля совпадают
+                        if (passReg == passReg2)
+                        //Используем базу данных для занесения пользователя в базу
+                        {
+                            using (hachiuebani123 hachi = new hachiuebani123())
+                            {
+                                //запрос к базе
+                                var query = hachi.auth.Where(x => x.login.Equals(loginReg)).FirstOrDefault();
+                                // если такого лоигна нет,то он вносится в базу
+                                if (query == null)
+                                //Добавление нового пользователя 
+                                {
+                                    hachi.auth.Add(new auth()
+                                    {
+                                        login = loginReg,
+                                        password = passReg,
+                                    }
+                                    );
+                                    //сохранение изменений
+                                    hachi.SaveChanges();
+                                    MessageBox.Show("Поздравляем!Вы успешно зарегестрировались!");
+                                    MainWindow Main = new MainWindow();
+                                    this.Close();
+                                    Main.Show();
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Такой логин уже существует");
+                                }
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Пароли не совпадают");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Недопустимые символы");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Пароль меньше 6 символов");
+                }
+
+            }
+
+        }
+
+        private void Button_Click_Help(object sender, RoutedEventArgs e)
+        {
+            Window2 reg = new Window2();
+            MessageBox.Show("Разрешенные символы для регистрации:Английский алфавит,Цифры от 0 до 9!");
+            this.Close();
+            reg.Show();
+        }
+
+
+        
+        private void Button_Click_Back(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            this.Close();
+            main.Show(); 
+        }
+    }
+}
+
+
+
